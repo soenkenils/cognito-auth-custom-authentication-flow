008f5107ef715c1f49e9ee777ff6814880e68b56
Sönke <soenke@element.in>
2019-02-08 11:25:40 +0100

    Add commented out intent for SMS_MFA usage

--- define-auth-challenge.js
+++ define-auth-challenge.js
@@ -9,7 +9,10 @@ module.exports.defineAuthChallenge = async (event, context, callback) => {
     event.response.issueTokens = false;
     event.response.failAuthentication = false;
     event.response.challengeName = "CUSTOM_CHALLENGE";
+    // Here is want I want but could not make work:
+    // event.response.challengeName = "SMS_MFA";
   } else if (event.request.session.length === 2 && event.request.session[1].challengeName == 'CUSTOM_CHALLENGE' && event.request.session[1].challengeResult == true) {
+  // } else if (event.request.session.length === 2 && event.request.session[1].challengeName == 'SMS_MFA' && event.request.session[1].challengeResult == true) {  
     event.response.issueTokens = true;
     event.response.failAuthentication = false;
   } else {
