c343bbf23f94745b2eaa3064de64b8923b4facfe
Sönke <soenke@element.in>
2019-02-08 11:09:53 +0100

    Remove bcrypt code

--- verify-auth-challenge/handler.js
+++ verify-auth-challenge/handler.js
@@ -1,15 +1,10 @@
 'use strict';
 
 module.exports.verifyAuthChallenge = async (event, context, callback) => {
-  if (event.request.privateChallengeParameters.answer.length > 6) {
-    const plaintextKey = event.request.challengeAnswer;
-    const encryptedKey = event.request.privateChallengeParameters.answer;
-    event.response.answerCorrect = await bcrypt.compare(plaintextKey, encryptedKey);
-  } else {
-    const sentPIN = event.request.privateChallengeParameters.answer;
-    const receivedPIN = event.request.challengeAnswer
-    event.response.answerCorrect = (sentPIN === receivedPIN);
-  }
+  const sentSecret = event.request.privateChallengeParameters.answer;
+  const receivedSecret = event.request.challengeAnswer;
+  
+  event.response.answerCorrect = (sentSecret === receivedSecret);
 
   callback(null, event);
 };
