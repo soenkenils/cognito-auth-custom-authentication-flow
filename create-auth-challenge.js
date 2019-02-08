2f9f2f4c7686d75cf0b685ed660a482dec9f4c1b
Sönke <soenke@element.in>
2019-02-08 11:17:16 +0100

    Move all handlers to top folder

--- create-auth-challenge.js
+++ create-auth-challenge.js
@@ -2,19 +2,28 @@
 
 module.exports.createAuthChallenge = async (event, context, callback) => {
   if (event.request.session.length === 0 && event.request.challengeName === 'CUSTOM_CHALLENGE') {
-    console.log('Email Challenge');
+    console.log('E-Mail Challenge');
+
+    const secret = '123456789abcdefg';
+
+    // send E-Mail
+
     event.response.publicChallengeParameters = {};
     event.response.privateChallengeParameters = {};
-    event.response.privateChallengeParameters.answer = "123456789abcdefg";
+    event.response.privateChallengeParameters.answer = secret;
     event.response.challengeMetadata = 'EMAIL_CHALLENGE';
-
   } else if (event.request.session.length === 1 && event.request.challengeName === 'CUSTOM_CHALLENGE') {
     console.log('SMS Challenge');
+
+    const code = '123456';
+
+    // send SMS
+
     event.response.publicChallengeParameters = {};
     event.response.privateChallengeParameters = {};
-    event.response.privateChallengeParameters.answer = '123456';
+    event.response.privateChallengeParameters.answer = code;
     event.response.challengeMetadata = 'SMS_CHALLENGE';
   }
-  
+
   callback(null, event);
 };
